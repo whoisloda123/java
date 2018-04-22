0f5e8711a93d21f872664a74d3355181d9cf91d7
liucan <553670590@qq.com>
2018-04-22 17:24:34 +0800

    增加java-learningrecodes学习java项目

---
+++ java-learningrecords/src/main/java/com/liucan/Utils.java
@@ -0,0 +1,9 @@
+package com.liucan;
+
+public class Utils {
+    public static void printMethodName() {
+        StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace();
+        StackTraceElement e = stacktrace[2];
+        System.out.println(e.getMethodName() + "--------------------------------------------------");
+    }
+}
---
+++ java-learningrecords/src/main/java/com/liucan/others/OtherTest.java
@@ -0,0 +1,102 @@
+package com.liucan.others;
+
+import com.liucan.Utils;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+
+public class OtherTest {
+    public void testAll() {
+        testTime(); //测试时间
+    }
+
+    /*
+    *java.util.Date和SimpleDateFormatter都不是线程安全的，java8而LocalDate和LocalTime和最基本的String一样，
+    * 是不变类型，不但线程安全，而且不能修改
+    * */
+    public void testTime() {
+        //参考https://my.oschina.net/benhaile/blog/193956
+        Utils.printMethodName();
+
+        //LocalDate
+        LocalDate date = LocalDate.now();
+        System.out.println("当前日期：" + date);
+        System.out.println("当前日期的年：" + date.getYear());
+        System.out.println("当前日期的月--枚举类型：" + date.getMonth());
+        System.out.println("当前日期的月--数字类型：" + date.getMonthValue());
+        System.out.println("当前日期是周几：" + date.getDayOfWeek());
+        System.out.println("当前日期是一年之中的第几天：" + date.getDayOfYear());
+
+        LocalDate christmas = LocalDate.of(2017, 12, 25);
+        System.out.println("christmas：" + christmas);
+
+        LocalDate endOfDec = LocalDate.parse("2017-12-28");
+        System.out.println("endOfDec：" + endOfDec);
+
+        //对日期进行加减 plus minus
+        System.out.println("当前日期plus一天：" + date.plusDays(1));
+        System.out.println("当前日期minus一天：" + date.minusDays(1));
+
+        //LocalTime
+        LocalTime now = LocalTime.now();
+        System.out.println("当前时间" + now);
+        System.out.println("当前时间：小时--" + now.getHour());
+        System.out.println("当前时间：分钟--" + now.getMinute());
+        System.out.println("当前时间：秒--" + now.getSecond());
+        //纳秒，不是毫秒，是十亿分之一秒。
+        System.out.println("当前时间：纳秒--" + now.getNano());
+        //你可以肆意设置时间的任意一个位置的值，使用withXXX()就可以啦。
+        System.out.println("当前时间：清空纳秒--" + now.withHour(1));
+        System.out.println("当前时间：挨个清零--" + now.withHour(1).withMinute(1).withSecond(1).withNano(1));
+
+        //LocalDateTime
+        //当前时间，以秒为单位
+        long epochSecond = System.currentTimeMillis() / 1000L;
+        //默认使用系统时区
+        ZoneId zoneId = ZoneOffset.systemDefault();
+        //之所以这么初始化，是因为根据传入的时间进行操作
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(epochSecond), zoneId);
+        //LocalDateTime.now();//也可以这么获得当前时间
+
+        System.out.println("localDateTime 初始化值：" + localDateTime);
+        System.out.println("getYear：" + localDateTime.getYear());
+        //方法返回值类型特殊，是枚举类型：Month类型
+        System.out.println("getMonth：" + localDateTime.getMonth());
+        System.out.println("getDayOfMonth：" + localDateTime.getDayOfMonth());
+        System.out.println("getSecond：" + localDateTime.getSecond());
+
+        //时间戳
+        long dayStart = localDateTime.atZone(zoneId).toEpochSecond();
+        System.out.println("dayStart 时间戳，秒数：" + dayStart);
+
+        localDateTime = LocalDateTime.of(2018, 4, 22, 15, 58, 0);
+        System.out.println("getDayOfWeek 的 ordinal 值：" + localDateTime.getDayOfWeek().ordinal());
+
+        /*
+         * 传入时间的年的第一天
+         */
+        LocalDateTime firstDayOfYear = localDateTime.with(TemporalAdjusters.firstDayOfYear());
+        System.out.println("firstDayOfYear：" + firstDayOfYear);
+        LocalDateTime plusDas = firstDayOfYear.plusDays(1);
+        System.out.println("plusDays：" + plusDas);
+
+        localDateTime = LocalDateTime.parse("2016-11-13 23:59", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        System.out.println(localDateTime);
+        //时间戳以及LocalDateTime和Instant相互转换
+        /*
+        * Instant对于之前的Date代表确定的时间点,且精确到纳秒
+        * */
+        Instant nowStamp = Instant.now();
+        System.out.println("nowStamp.getEpochSecond " + nowStamp.getEpochSecond() + ":" +nowStamp.getNano());
+
+        //Instant to LocalDateTime
+        localDateTime = LocalDateTime.ofInstant(nowStamp, ZoneOffset.systemDefault());
+        System.out.println("Instant to LocalDateTime：" + localDateTime);
+
+        //LocalDataTime to Instant
+        nowStamp = localDateTime.toInstant(ZoneOffset.of("+8"));
+        System.out.println("LocalDateTime to Instant：" + nowStamp.getEpochSecond());
+        System.out.println("localDateTime.toEpochSecond：" + localDateTime.toEpochSecond(ZoneOffset.of("+8")));
+    }
+}
