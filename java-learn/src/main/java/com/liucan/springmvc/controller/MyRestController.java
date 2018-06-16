d69863ba1b1ae327f056dd69a99948ae3d82193e
liucan <553670590@qq.com>
2018-06-16 19:24:25 +0800

    增加stream.collect的分组和分区的学习

---
+++ java-learn/src/main/java/com/liucan/springmvc/controller/MyRestController.java
@@ -0,0 +1,33 @@
+package com.liucan.springmvc.controller;
+
+import com.liucan.mybatis.dao.UserInfoMapper;
+import com.liucan.mybatis.mode.UserInfo;
+import com.liucan.mybatis.mode.UserInfoExample;
+import com.liucan.springmvc.common.response.CommonResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author liucan
+ * @date 2018/6/16
+ * @brief @RestController = @Controller + @ResponseBody
+ */
+@RestController //一般用于返回页面
+@RequestMapping("/liucan")
+public class MyRestController {
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
+    @GetMapping(value = "/find_user1")
+    public CommonResponse queryUser(@RequestParam("user_id") Integer userId) {
+        UserInfoExample userInfoExample = new UserInfoExample();
+        userInfoExample.createCriteria().andUserIdEqualTo(userId);
+        List<UserInfo> list = userInfoMapper.selectByExample(userInfoExample);
+        return CommonResponse.ok(list.get(0));
+    }
+}
