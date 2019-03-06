0cb8478c12dd14b1df506f4de91a674bf7302f9a
liucan <553670590@qq.coom>
2019-03-06 10:13:54 +0800

    增加mybatis分页插件PageHelper的学习

--- java-learn/src/main/java/com/liucan/mybatis/Mybatis.java
+++ java-learn/src/main/java/com/liucan/mybatis/Mybatis.java
@@ -18,6 +18,14 @@ import java.io.InputStream;
  * 1.SqlSessionFactory 一旦被创建就应该在应用的运行期间一直存在，没有任何理由对它进行清除或重建
  *  两个数据库需要创建两个SqlSessionFactory 实例，每个数据库对应一个
  * 2.每个线程都应该有它自己的 SqlSession 实例。SqlSession 的实例不是线程安全的，因此是不能被共享的
+ *
+ * 3.PageHelper是Mybatis数据库物理分页插件，是对mybatis执行流程进行了增强，添加了limit和count属于物理分页
+ *  可以直接获取分页内容和总数
+ *  PageHelper的原理是基于拦截器实现的。拦截器的配置有两种方法，一种是在mybatis的配置文件中配置，一种是直接在spring的配置文件中进行
+ *  相当于在真正查询的时候会把分页传进去查询
+ *  直接用PageHelper.startPage(currentPage, pageSize).doSelect()就可以了
+ *  参考：https://www.cnblogs.com/digdeep/p/4608933.html
+ *
  */
 @Component
 public class Mybatis {
