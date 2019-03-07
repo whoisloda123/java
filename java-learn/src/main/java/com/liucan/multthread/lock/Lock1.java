132d7f86fb2d26ee790dc49e108d2c45774c93fd
553650590@qq.com <553650590@qq.com>
2019-03-07 22:55:49 +0800

    增加Executors创建ThreadPoolExecutor的4种线程池的学习

--- java-learn/src/main/java/com/liucan/multthread/lock/Lock1.java
+++ java-learn/src/main/java/com/liucan/multthread/lock/Lock1.java
@@ -10,9 +10,12 @@ import java.util.concurrent.locks.*;
  * @version 19-3-5
  */
 public class Lock1 {
+    //可重入锁
     private final Lock reentrantLock = new ReentrantLock();
+    //条件变量
     private final Condition condition = reentrantLock.newCondition();
 
+    //读写锁
     private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
     private final Lock readLock = readWriteLock.readLock();
     private final Lock writeLock = readWriteLock.writeLock();
