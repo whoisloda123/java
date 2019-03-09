f8ddd12cc33ae133630fde0ddc2d2a1524dd3104
553650590@qq.com <553650590@qq.com>
2019-03-09 23:39:20 +0800

    增加Exchanger的学习

--- java-learn/src/main/java/com/liucan/multthread/cyclicbarrier/CyclicBarrier1.java
+++ java-learn/src/main/java/com/liucan/multthread/cyclicbarrier/CyclicBarrier1.java
@@ -6,7 +6,7 @@ import java.util.concurrent.CyclicBarrier;
  * 栅栏
  * 所有线程必须在栅栏旁边等待其他线程，直到所有线程到达才能继续做其他事情
  * 可以通过传入参数设置所有线程到达栅栏后执行一个Runnable
- *
+ * 它可以协同多个线程，让多个线程在这个屏障前等待，直到所有线程都达到了这个屏障时，再一起继续执行 后面的动作
  * @author liucan
  * @version 19-3-3
  */
