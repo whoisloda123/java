c980d0fc4cadb127bd53d84973db3d681871ec43
553650590@qq.com <553650590@qq.com>
2019-04-07 22:38:04 +0800

    优化代码

--- java-learn/src/main/java/com/liucan/designpattern/creativepattern/singleton/LazySingleton.java
+++ java-learn/src/main/java/com/liucan/designpattern/creativepattern/singleton/LazySingleton.java
@@ -14,6 +14,17 @@ public class LazySingleton {
     private LazySingleton() {
     }
 
+    /**
+     * 可用于单例模式反序列化时生成新的对象
+     * JVM从内存中反序列化地"组装"一个新对象时,就会自动调用这个 readResolve方法来返回我们指定好的对象了, 单例规则也就得到了保证.
+     */
+    private Object readResolve() {
+        // instead of the object we're on,
+        // return the class variable INSTANCE
+        return instance;
+
+    }
+
     public synchronized static LazySingleton getInstance() {
         if (instance == null) {
             instance = new LazySingleton();
--- java-learn/src/main/java/com/liucan/pojo/Person.java
+++ java-learn/src/main/java/com/liucan/pojo/Person.java
@@ -46,6 +46,17 @@ public class Person implements Serializable, Cloneable {
     }
 
     /**
+     * 可用于单例模式反序列化时生成新的对象
+     * JVM从内存中反序列化地"组装"一个新对象时,就会自动调用这个 readResolve方法来返回我们指定好的对象了, 单例规则也就得到了保证.
+     */
+    private Object readResolve() {
+        // instead of the object we're on,
+        // return the class variable INSTANCE
+        return this;
+
+    }
+
+    /**
      * 深拷贝：先将对象序列化然后再反序列化
      */
     @Override
