2eb0537a3bb1c9c70b7478534ddc69dbcd902f9c
553650590@qq.com <553650590@qq.com>
2019-06-20 22:17:36 +0800

    增加nio,bio,aio的学习

--- java-learn/src/main/java/com/liucan/other/Document.java
+++ java-learn/src/main/java/com/liucan/other/Document.java
@@ -577,7 +577,15 @@ public class Document {
      *
      *   52.nio,aio,bio的区别
      *     https://www.cnblogs.com/barrywxx/p/8430790.html
-     *      1.BIO:
+     *     https://blog.csdn.net/lisha006/article/details/82856906
+     *      1.BIO:同步阻塞模式
+     *      2.NIO：同步非阻塞模式
+     *          a.有selector，channel（通道：可写可读），buffer（载体）
+     *          b.采用IO多路复用模型，epoll模型代替select模型
+     *              select模型：采用轮寻方式，轮寻到有准备好的io后，返回进行后续操作
+     *              epoll模型：采用注册通知方式，当有准备好的io后，会回调通知相应的处理
+     *      3.AIO:异步非阻塞模式
+     *          不需要轮询等待，而是通过操作系统处理后，会自动通知服务处理相应的请求，相对于轮训操作交给操作系统
      *
      *  学习方向？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？
      *  https://www.cnblogs.com/szlbm/p/5437498.html
