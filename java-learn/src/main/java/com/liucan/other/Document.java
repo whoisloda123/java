8f82f660702e6572883d9a5ec3bee39b50741cb6
553650590@qq.com <553650590@qq.com>
2019-07-22 23:18:49 +0800

    增加spring如何解决循环依赖问题的学习

--- java-learn/src/main/java/com/liucan/other/Document.java
+++ java-learn/src/main/java/com/liucan/other/Document.java
@@ -794,7 +794,7 @@ public class Document {
      *
      *  68.如何防止sql注入？
      *  https://www.cnblogs.com/jiangxueqiao/p/7444127.html
-     *      a.转义字符特殊处理
+     *      a.转义字符特殊处理，在mysql里面具有特殊含义的字符转义一下
      *      b.Prepared Statement预处理语句(如mybatis#{} 预编译阶段用?代替,然后正在执行的时候在替换)
      *
      *  69.高可用高并发一般方案是什么？7种
@@ -851,6 +851,20 @@ public class Document {
      *
      *  76.rpc框架
      *  http://youzhixueyuan.com/implementation-principle-of-rpc-framework.html
+     *
+     *  77.spring如何解决循环依赖的？
+     *  https://blog.csdn.net/lzhcoder/article/details/84144381
+     *      a.场景：
+     *          属性循环依赖：可以解决
+     *          构造器依赖：不能解决
+     *      b.如何检测：
+     *          Bean在创建的时候可以给该Bean打标，如果递归调用回来发现正在创建中的话，即说明了循环依赖了。
+     *      c.属性循环依赖如何解决
+     *          1.单例对象创建过程：实例化->填充属性->初始化
+     *          2.在创建A的时候会把A对应的ObjectFactory放到缓存中，在创建A的过程，发现依赖B，然后创建B，发现B
+     *              里面依赖A，这个时候如果发现A正在创建中，会从缓存里面拿到创建好的A（没有填充属性）
+     *          3.实例化完成后，会填充属性，初始化
+     *
      *
      * dubbo?
      * tcp滑动窗口
