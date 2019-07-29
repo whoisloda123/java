0024f3dfa3e5abf723cc9938eedc2d355038a035
553650590@qq.com <553650590@qq.com>
2019-07-29 22:56:56 +0800

    增加项目学习

---
+++ java-learn/src/main/java/com/liucan/other/XiangMu.java
@@ -0,0 +1,27 @@
+package com.liucan.other;
+
+/**
+ * @author liucan
+ * @version 19-7-29
+ */
+public class XiangMu {
+    /*
+     *
+     *
+     * javawiress
+     * 1.thrift
+     * 3.common
+     * 7.扭蛋活动
+     * 8.kafka
+     * 9.下单抽免单
+     * 10.加购抽奖-大促活动
+     *  a.kafka消费用户的加入购物车消息，保存到mysql
+     *  b.redis保存有券的信息
+     *  c.通过quartz任务调度系统执行定时任务
+     *  d.通过thrift调用券服务的查询券信息和发券
+     *  e.通过mysql-mybatis保存用户发券记录
+     *  f.活动的一些信息，如开始结束时间保存在zk里面
+     *  g.后台管理系统配置券信息
+     *
+     */
+}
